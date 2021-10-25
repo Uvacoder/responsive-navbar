927d1f6cf416b83deb3a46c8f7d605e6878c0c26
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-10-24 20:41:53 -0600

    lit is no longer external

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
       entry: 'src/responsive-navbar.ts',
       formats: ['es']
     },
-    rollupOptions: {
-      external: /^lit/
-    }
+    // rollupOptions: {
+    //   external: /^lit/
+    // }
   }
 })
