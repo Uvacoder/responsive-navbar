9102d0bcd05a9e3ce6dbd538d3ff3e08bff81351
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-10-27 23:56:32 -0600

    added action items slot

--- src/responsive-navbar.ts
+++ src/responsive-navbar.ts
@@ -2,32 +2,34 @@ import { html, css, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { menuIcon, closeIcon } from "./icons";
 
-export const tagName = 'responsive-navbar';
+export const tagName = "responsive-navbar";
 
 @customElement("responsive-navbar")
 export class ResponsiveNavbar extends LitElement {
   static styles = css`
     header {
       width: 100%;
-      min-height: 64px;
       display: flex;
+      background-color: var(--navbar-background-color, #ffffff);
     }
     .nav {
       width: 100%;
-      /* padding: 0 1rem; */
       display: flex;
       align-items: center;
       background-color: var(--navbar-background-color, #ffffff);
     }
     .nav--align-right {
-      justify-content: space-between
+      justify-content: space-between;
+    }
+    .action-items__section {
+      display: flex;
+      align-self: flex-end;
     }
     #menu-icon__button {
       display: none;
     }
     @media only screen and (max-width: 475px) {
       .nav {
-        padding: 0.5rem 2rem 0.5rem 0;
         justify-content: initial;
       }
       #menu-icon__button {
@@ -42,11 +44,11 @@ export class ResponsiveNavbar extends LitElement {
       }
       .navlinks__container {
         width: 100%;
-        height: 91vh;
+        height: 93vh;
         display: none;
         background-color: var(--menu-background-color, #ffffff);
         position: absolute;
-        top: 9vh;
+        top: 7vh;
         right: 0;
         bottom: 0;
         left: 0;
@@ -62,27 +64,22 @@ export class ResponsiveNavbar extends LitElement {
   // @property({ type: Boolean, reflect: true, attribute: "sticky" })
   // sticky = false;
 
-  // TODO add property to change location of desktop nav
-  // @property({ type: String })
-  // menuAlign = "left";
-
   @property({ type: Boolean, reflect: true, attribute: "menu-open" })
   menuOpen: boolean = false;
 
   @property({ type: String, reflect: true, attribute: "nav-align" })
-  navAlign:string = 'left';
+  navAlign: string = "left";
 
   render() {
     return html` <header>
-      <nav class="nav ${this.navAlign === 'right' ? 'nav--align-right' : ''}">
+      <nav class="nav ${this.navAlign === "right" ? "nav--align-right" : ""}">
         <button id="menu-icon__button" @click=${this._toggleMenu}>
           ${this.menuOpen
             ? html`<slot name="close-icon"><span>${closeIcon}</span></slot>`
             : html`<slot name="menu-icon"><span>${menuIcon}</span></slot>`}
         </button>
         <div class="logo__container">
-          <slot name="logo">
-          </slot>
+          <slot name="logo"> </slot>
         </div>
         <div
           class="navlinks__container ${this.menuOpen
@@ -92,12 +89,15 @@ export class ResponsiveNavbar extends LitElement {
           <slot name="navlinks"></slot>
         </div>
       </nav>
+      <section class="action-items__section">
+        <slot name="action-items"></slot>
+      </section>
     </header>`;
   }
 
   private _toggleMenu() {
     this.menuOpen = !this.menuOpen;
-    document.body.toggleAttribute('no-scroll');
+    document.body.toggleAttribute("no-scroll");
   }
 }
 
